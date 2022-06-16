d538a7b926185e5f6db5f893cd6fbb44c110840e
vikash <kumarvikashv681@gmail.com>
2022-06-16 23:15:19 +0530

    optimised server and added one more route

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+// requiring date function
+const d = require(path.join(__dirname, 'modules/date.js'));
 
 const app = express();
-app.user("view engine", "ejs");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+let items = [];
+let workItems = [];
 
 app.get("/", (req, res) => {
-    const d = new Data();
-    const day = d.getDay;
-    let weekDay_or_Not = "";
-    if(day === 6 || day === 0){
-        weekDay_or_Not = "weekend";
+
+    const day  = d();
+    
+    res.render('list', { listTitle: day, itemList:items});
+});
+
+app.post("/",(req,res)=>{
+    if(req.body.list == "work"){
+        console.log("i am in redirecting");
+        const item = req.body.newItem;
+        workItems.push(item);
+        res.redirect("/work");
     }
     else{
-        weekDay_or_Not = "Working Day";
+    const item = req.body.newItem;
+    items.push(item)
+    res.redirect("/");
     }
+
 })
+
+app.get("/work",(req,res)=>{
+    res.render('list',{listTitle:"work", itemList:workItems});
+});
+
 
 app.listen(8080);
