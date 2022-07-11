89cfa324c141ffe18506caf14a1c8263cb6cd792
vikash <kumarvikashv681@gmail.com>
2022-07-11 07:41:31 +0530

    note insertion, redering and deletion fucntionality implemented

--- app.js
+++ app.js
@@ -1,42 +1,83 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// requiring date function
+
+// requiring date function from date.js
 const d = require(path.join(__dirname, 'modules/date.js'));
+
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [];
+
+
+
+// connecting to data base 
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/noteapp");
+
+const appSchema = mongoose.Schema({
+    note: {
+        type: String,
+        required: true
+    }
+})
+
+// model 
+const note = mongoose.model('note', appSchema);
+
+// let items = [];
 let workItems = [];
 
-app.get("/", (req, res) => {
 
-    const day  = d();
-    
-    res.render('list', { listTitle: day, itemList:items});
+// routing
+app.get("/", (req, res) => {
+    const day = d()
+    note.find().then((items)=>{
+        res.render('list', { listTitle: day, itemList: items });
+    })
+    .catch(err => console.log(err));
 });
 
-app.post("/",(req,res)=>{
-    if(req.body.list == "work"){
-        console.log("i am in redirecting");
+// Inserting data 
+app.post("/", (req, res) => {
+    if (req.body.list == "work") {
+        // console.log("i am in redirecting");
         const item = req.body.newItem;
         workItems.push(item);
         res.redirect("/work");
     }
-    else{
-    const item = req.body.newItem;
-    items.push(item)
-    res.redirect("/");
+    else {
+        const item = req.body.newItem;
+        const todo = new note({
+            note:item
+        })
+        todo.save();
+        res.redirect("/");
     }
 
 })
 
-app.get("/work",(req,res)=>{
-    res.render('list',{listTitle:"work", itemList:workItems});
+// deleting data from data base 
+app.post("/delete", (req, res) => {
+    const delId = req.body.delItem;
+    // console.log(typeof(delId));
+    note.deleteOne({id:delId},(err)=>{
+        if (err) 
+            console.log(err);
+    });
+    res.redirect("/");
+
+})
+
+
+app.get("/work", (req, res) => {
+    res.render('list', { listTitle: "work", itemList: workItems });
 });
 
 
-app.listen(8080);
+app.listen(8000);
